d16bb078af47556435f62dc18d7e73d1e26d406d
smunoz <smunoz@ebay.com>
2013-04-19 11:19:10 +0200

    Changed api key

--- app/models/GcmService.java
+++ app/models/GcmService.java
@@ -9,8 +9,8 @@ import java.io.IOException;
 
 public class GcmService {
 
-//    private static final String API_KEY = "AIzaSyD788SjyS_FByYToA1DmGxs7Od9W79khwI";
-    private static final String API_KEY = "AIzaSyCRdiIh9H6EpLp_JvZ9cmH6mtGf1KhuVwc";
+    private static final String API_KEY = "AIzaSyD788SjyS_FByYToA1DmGxs7Od9W79khwI";
+//    private static final String API_KEY = "AIzaSyCRdiIh9H6EpLp_JvZ9cmH6mtGf1KhuVwc";
 
     public static void sendMessage(String regId, String adId) throws IOException {
         Sender sender = new Sender(API_KEY);
